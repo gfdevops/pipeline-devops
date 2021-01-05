190c36883ea00b7d3f58e45999399a0a3f331ba9
Gerardo Felmer <desarrollo@satelnet.cl>
2021-01-05 15:46:15 -0300

    se agrega maven

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -46,13 +46,13 @@ pipeline {
                             if (stagesLowercase.contains("build") || stagesLowercase.contains("test")) {
                                 //si NO contiene RUN, pero si rest o nexus
                                 if (!stagesLowercase.contains("run") && 
-                                    ( stagesLowercase.contains("rest") || stagesLowercase.contains("nexus") )) {
-                                    env.ERROR_MESSAGE = "Es necesario ejecutar el stage run si se quiere correr rest o nexus"
+                                    ( stagesLowercase.contains("rest"))) {
+                                    env.ERROR_MESSAGE = "Es necesario ejecutar el stage run si se quiere correr rest"
                                     error(env.ERROR_MESSAGE)
                                 }
                                 //se ejecutan en orden, se toman los validos, se chequea que existan y se ejecutan
                                 for (String item : valid_stages_gradle) {
-                                    println("Ejecutando stage => "+item);
+                                    println("Ejecutando stage gradle => "+item);
                                     if (stagesLowercase.contains(item)) {
                                         gradle.call(item)
                                     }
@@ -65,41 +65,55 @@ pipeline {
                         }
                     }
 
+
                     if (params.HERRAMIENTA == 'maven') {
-                        // if (params.stage == '') {
-                        //         maven.call()
-                        // }else {
-                        //     def valid_stages_maven = ["build","test","jar","sonarqube","upload_nexus"]
-                        //     def stagesList = params.stage.tokenize(";")
-                        //     def stagesLowercase = stagesList.collect{ it.toLowerCase() }
+                        //se definen los stages validos para maven
+                        def valid_stages_maven = ["build","test","jar","sonarqube","upload_nexus"]
+                        //se separan los stages por punto y coma
+                        def stagesLowercase = params.stage.tokenize(";").collect{ it.toLowerCase() }
+                        //se pasan los stages ingresados a minusculas
+                        //def stagesLowercase = stagesList.collect{ it.toLowerCase() }
 
-                        //     if (valid_stages_maven.containsAll(stage)) {
-                                
-                        //     }else {
-                        //         println("No existe uno de los stage ingresados en el proyecto maven")
-                        //     }
-
-                        //     //Build
-                        //     //Test
-                        //     //Jar
-                        //     //SonarQube
-                        //     //Upload Nexus
-
-                        // }
+                        for (String item : stagesLowercase) {
+                            //si valida si el stage se encuentra dentro de los validos
+                            if (!valid_stages_maven.contains(item)) {
+                                env.ERROR_MESSAGE = "El stage ${item} no es valido para proyecto maven"
+                                error(env.ERROR_MESSAGE)
+                            }
+                        }
+                        //aqui todos los stages ingresados fueron validados
+                        //si no se le pasa stages, se corren todos
+                        if (stagesLowercase.size() == 0) {
+                            maven.call('all')
+                        //si solo se pasa 1, ese debe correr
+                        } else if (stagesLowercase.size() == 1) {
+                            //según el ejercicio, debiese ejecutarse cuando es 1, por eso no se le agregan validaciones
+                            maven.call(stagesLowercase.get(0))          
+                        //si se le pasa varios, se ejecutan secuencial y en orden coherente
+                        } else {
+                            //si build está en la lista todo OK hasta RUN
+                            //si build no está en la lista problemas !
+                            if (stagesLowercase.contains("build") || stagesLowercase.contains("jar")) {
+                                //si NO contiene RUN, pero si rest o nexus
+                                if ( (!stagesLowercase.contains("build") || !stagesLowercase.contains("jar"))  && 
+                                    ( stagesLowercase.contains("sonarqube")  ||  stagesLowercase.contains("upload_nexus")  )) {
+                                    env.ERROR_MESSAGE = "Es necesario ejecutar el stage build y jar, antes de ejecutar sonarqube o upload_nexus"
+                                    error(env.ERROR_MESSAGE)
+                                }
+                                //se ejecutan en orden, se toman los validos, se chequea que existan y se ejecutan
+                                for (String item : valid_stages_maven) {
+                                    println("Ejecutando stage maven => "+item);
+                                    if (stagesLowercase.contains(item)) {
+                                        maven.call(item)
+                                    }
+                                    //item.contains(stagesLowercase) ? gradle.call(item) : continue
+                                }
+                            }else {
+                                env.ERROR_MESSAGE = "Primero es necesario ejecutar el stage build o test"
+                                error(env.ERROR_MESSAGE)
+                            }
+                        }
                     }
-
-                    // if (params.buildtool == 'gradle') {
-                    //     gradle.call()
-                    // }else {
-                    //     maven.call()
-                    // }
-
-                    // if (params.HERRAMIENTA == 'gradle') {
-                    //     ejecucion = load 'gradle.groovy'
-                    // }else  {
-                    //     ejecucion = load 'maven.groovy'
-                    // }
-                    // ejecucion.call()
                 }
             }
         }
